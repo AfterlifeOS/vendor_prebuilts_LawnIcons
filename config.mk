ea12937bfb98836819c5545912c35f0f0f29dd93
HinohArata <mmgcntk@gmail.com>
2024-12-06 15:06:08 +0700

    LawnIcons: Re-Arrange code for build LawnIcons
    Change-Id: If9f62d338692f63aaf2817d019eed301dec2fdec
    Signed-off-by: HinohArata <mmgcntk@gmail.com>

--- config.mk
+++ config.mk
@@ -1,3 +1,6 @@
-# Build
-PRODUCT_PACKAGES += \
-    LawnIcons
+LOCAL_PATH := $(call my-dir)
+include $(call all-subdir-makefiles,$(LOCAL_PATH))
+
+# Include prebuilts packages
+include vendor/prebuilts/LawnIcons/product/prebuilts.mk
+RELAX_USES_LIBRARY_CHECK=true
---
+++ product/prebuilts.mk
@@ -0,0 +1,4 @@
+# Build apps
+$(warning "Your build will shipped with LawnIcons")
+PRODUCT_PACKAGES += \
+    LawnIcons
